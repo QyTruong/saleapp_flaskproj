e4bdcaaba6cbc6042f6b8ceb886216cc72447549
admin <truong.4725212@gmail.com>
2025-11-01 16:21:51 +0700

    update code

--- app/saleapp/index.py
+++ app/saleapp/index.py
@@ -1,9 +1,14 @@
-from flask import render_template
+from flask import render_template, request
 from app.saleapp import app
+import dao
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    categories = dao.load_categories()
+    products = dao.load_products(cate_id=request.args.get('category_id'), kw=request.args.get('kw'))
+
+    return render_template('index.html', categories=categories, products=products)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
